e96f131fa460f11135359069bc90da00fa428d22
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-09 16:52:55 +0700

    alternatif cara deklarasi instance variable

--- internet-banking-frontend/src/app/rekening/rekening.model.ts
+++ internet-banking-frontend/src/app/rekening/rekening.model.ts
@@ -1,15 +1,9 @@
 export class Rekening {
 
-  id: string;
-  nomer: string;
-  nama: string;
-
   constructor(
-    id: string,
-    nomer: string,
-    nama: string){
-      this.id = id;
-      this.nomer = nomer;
-      this.nama = nama;
+    public id: string,
+    public nomer: string,
+    public nama: string){
+      
   }
 }
