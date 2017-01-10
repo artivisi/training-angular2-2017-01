3853444930e957563c1bab2f10e74aa1d53796ab
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-10 14:21:39 +0700

    implement combo pilihan rekening

--- internet-banking-frontend/src/app/rekening/pilihan-rekening/pilihan-rekening.component.ts
+++ internet-banking-frontend/src/app/rekening/pilihan-rekening/pilihan-rekening.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Rekening } from '../rekening.model';
+import { RekeningService } from '../rekening.service';
+
 @Component({
   selector: 'pilihan-rekening',
   templateUrl: './pilihan-rekening.component.html',
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PilihanRekeningComponent implements OnInit {
 
-  constructor() { }
+  daftarRekening : Rekening[];
+
+  constructor(private rekeningService : RekeningService) {
+      rekeningService.ambilDataRekening()
+      .then(hasil => this.daftarRekening = hasil)
+      .catch(this.handleError);
+  }
+
+  private handleError(errors: any): void {
+      console.log("Terjadi error : "+errors);
+  }
 
   ngOnInit() {
   }
