e4101ac22a3962b8f1d8b656fd9e0c27a1dea82f
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-11 14:46:29 +0700

    enable oauth server

--- internet-banking-backend/src/main/java/id/artivisi/training/angular2/InternetBankingBackendApplication.java
+++ internet-banking-backend/src/main/java/id/artivisi/training/angular2/InternetBankingBackendApplication.java
@@ -2,8 +2,10 @@ package id.artivisi.training.angular2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class InternetBankingBackendApplication {
 
 	public static void main(String[] args) {
---
+++ internet-banking-backend/src/main/java/id/artivisi/training/angular2/KonfigurasiSecurity.java
@@ -0,0 +1,83 @@
+package id.artivisi.training.angular2;
+
+import java.security.KeyPair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+@EnableWebSecurity(debug = true)
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("endy").password("123").roles("ADMIN").and()
+                .withUser("anton").password("123").roles("CUSTOMER");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class KonfigurasiAuthServer extends
+            AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        @Qualifier("authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
+
+        @Bean
+        public JwtAccessTokenConverter jwtAccessTokenConverter() {
+            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+            KeyPair keyPair = new KeyStoreKeyFactory(
+                    new ClassPathResource("jwt.jks"), "rahasia".toCharArray())
+                    .getKeyPair("jwt");
+            converter.setKeyPair(keyPair);
+            return converter;
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+                throws Exception {
+            endpoints
+                    .accessTokenConverter(jwtAccessTokenConverter())
+                    .authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            oauthServer
+                    .tokenKeyAccess("permitAll()")
+                    .checkTokenAccess("hasRole('CLIENT')");
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients
+                    .inMemory()
+                    .withClient("internetbanking")
+                    .secret("123456")
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "implicit", "password")
+                    .scopes("read", "write")
+                    .autoApprove(true)
+                    .resourceIds("belajarangular");
+        }
+    }
+}
